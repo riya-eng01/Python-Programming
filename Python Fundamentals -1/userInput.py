37b29e2264efe5ba88c1f984bf724c7559626ebd
Riya Singh <riyasingheng01@gmail.com>
2026-08-07 20:40:14 +0530

    Modified file

--- Python Fundamentals -1/userInput.py
+++ Python Fundamentals -1/userInput.py
@@ -4,3 +4,9 @@ print(a)
 
 b = input("Enter your name: ")
 print(b)
+
+# Take user input to add two numbers
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+sum = num1 + num2 
+print("Sum = " , sum)

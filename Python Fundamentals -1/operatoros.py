19383d07f8b1fcfd552da2db0db3b5ebb408ce23
Riya Singh <riyasingheng01@gmail.com>
2026-08-07 22:14:56 +0530

    Python files

---
+++ Python Fundamentals -1/operatoros.py
@@ -0,0 +1,52 @@
+# Operators in python
+
+# Arithmetic operators
+a = 34
+b = 2
+print("a + b = ", a + b)
+print("a - b = ", a - b)
+print("a * b = ", a * b)
+print("a / b = ", a / b)
+print("a % b = ", a % b)
+print("a // b = ", a // b) # floor division - Remove decimals
+print("a ** b = ", a**b)
+
+# Conditional operators
+print(a > 4)
+print(a < 4)
+print(a <= 4)
+print(a >= 4)
+print(a == 4) # Is a equal to 4?
+print(a == 34)
+print(a != 34) # Not equal to
+
+# Logical Operators
+print("For and operator...")
+print(True and True)
+print(True and False)
+print(False and True)
+print(False and False)
+
+print("For or operator....")
+print(True or True)
+print(True or False)
+print(False or True)
+print(False or False)
+
+print("For not operator")
+print(not True)
+print(not False)
+
+# Assignment operators
+x = 32
+print(x)
+x += 3
+print(x)
+x -= 5
+print(x)
+x *= 3
+print(x)
+x /= 3
+print(x)
+x **= 2 # exponentiation assignment operator
+print(x)

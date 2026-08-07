4ebb08e9de135b89eb5f9b8f868ab17d2d07e657
Riya Singh <riyasingheng01@gmail.com>
2026-08-07 22:40:09 +0530

    Add python files

---
+++ Python Fundamentals -1/practiceSet.py
@@ -0,0 +1,55 @@
+# Python Practice Set
+# Ques-1
+print("Hello, World! Welcome to Python.")
+
+# Ques-2
+print("Twinkle Twinkle, little star\nHow I wonder what you are!")
+
+# Ques-3
+name = "Riya Singh"
+age = 22
+height = 4.11
+is_student = True
+# print(name, age, height, is_student)
+print("name =", name, end=", ")
+print("age =", age, end=", ")
+print("height =", height, end=", ")
+print("is_student =", is_student)
+
+# Ques-4
+num = "45"
+num = int(num)
+num += 10
+print(num)
+
+# Ques-5
+fav_food = input("Enter your favourite food: ")
+print("WOW! I also like", fav_food)
+
+# Ques-6
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+print("Sum = ", num1 + num2)
+print("Difference = ", num1 - num2)
+print("Product = ", num1 * num2)
+print("Quotient = ", num1 / num2)
+
+# Ques-7
+print("Hello \"Python\" World!\nThis is on a new line")
+print("This is a tab ->\tafter tab.")
+
+# Ques-8
+number = int(input("Enter a number: "))
+print("Square:", number**2)
+print("Cube:", number**3)
+
+# Q9: Quick Quiz (True/False)
+# Mark each as True or False:
+
+# 1.Python code must always end with a semicolon ; -> False
+# 2.The # symbol is used for comments in Python -> True
+# 3."123" and 123 are the same in Python -> False
+# 4.The * operator is used for multiplication -> True
+# 5.\n creates a new line -> True
+# 6.Variables in Python can start with numbers -> False
+# 7.int("10") + 5 gives 15 -> True

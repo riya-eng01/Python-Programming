e2d3aa2f9a54a2572d4e278b760a74f6cd575f71
Riya Singh <riyasingheng01@gmail.com>
2026-08-07 21:40:04 +0530

    New file

---
+++ Python Fundamentals -1/comments.py
@@ -0,0 +1,32 @@
+print("Hey How are you")
+
+# Riya please finish this code
+
+'''
+This
+is a Multi-line
+comment
+'''
+
+# Escape Sequence Characters
+print("hey how are you \nI am good")
+print("single backslash \\")
+print("\"Riya\"") # double quotes
+print("\'Single quotes\'")
+print("Heyy!! \t Helloo..") # space of tab
+
+# print() function is used to display output
+print('"Hello world"')
+print('Hello world',"Harry",3) # default spaces will be printed
+print("Print statement")
+print("Riya")
+
+# To print commas instead of spaces
+print('Hello world', "Riya", 3, sep=",") # Overriding default behave
+print('Hello world', "Riya", 3, sep="|")
+
+# Overriding end of print statement
+# print("This is ", end="\n") # default
+# print("This is ", end=",")
+print("This is ", end="..")
+print("Overriden")

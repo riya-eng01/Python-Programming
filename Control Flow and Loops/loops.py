c12f0cb74c96475faf39e3d6baa383e5fa3926f3
Riya Singh <riyasingheng01@gmail.com>
2026-08-10 22:52:13 +0530

    Python file

---
+++ Control Flow and Loops/loops.py
@@ -0,0 +1,12 @@
+# print(1)
+# print(2)
+# print(3)
+
+# For loops
+for i in range(1, 6): # range function goes from 1 to (6-1) ie 5 in this case
+    print(i)
+
+# print the table of 5
+num = 5
+for i in range(1, 11):
+    print(num, "X", i, "=", (num*i))

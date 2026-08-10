b6be86720276d29e88dd156645b02a84fa851b15
Riya Singh <riyasingheng01@gmail.com>
2026-08-10 22:42:16 +0530

    match cases

--- Control Flow and Loops/match_case.py
+++ Control Flow and Loops/match_case.py
@@ -1,1 +1,12 @@
 # Match case Statements in Python
+a = int(input("Enter a number between 1 and 10: "))
+
+match(a):
+    case 1:
+        print("You won a charger")
+    case 3:
+        print("You won $3")
+    case 6:
+        print("You won a camera!")
+    case _:    # default case
+        print("Better luck next time")

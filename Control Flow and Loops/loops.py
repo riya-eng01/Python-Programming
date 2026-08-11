bf5d782984a38979ebed4935d49b4c293c5cc694
Riya Singh <riyasingheng01@gmail.com>
2026-08-11 23:31:50 +0530

    for loop

--- Control Flow and Loops/loops.py
+++ Control Flow and Loops/loops.py
@@ -10,3 +10,8 @@ for i in range(1, 6): # range function goes from 1 to (6-1) ie 5 in this case
 num = 5
 for i in range(1, 11):
     print(num, "X", i, "=", (num*i))
+
+# Print even Nums
+for i in range(1, 11):
+    if (i % 2 == 0):
+        print(i)

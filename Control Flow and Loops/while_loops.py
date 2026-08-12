1e808846e0322c9262d1c0630ad589b430b9bad1
Riya Singh <riyasingheng01@gmail.com>
2026-08-12 16:24:34 +0530

    while loops

--- Control Flow and Loops/while_loops.py
+++ Control Flow and Loops/while_loops.py
@@ -1,1 +1,20 @@
 # while loops
+i = 1
+while (i < 6):
+    print(i)
+    i = i + 1
+
+print("Out of while loop!")
+
+# Print even numbers from 1 - 10 using while loop
+counter = 1
+while(counter <= 10):
+    if(counter % 2 == 0):
+        print(counter)
+    counter = counter + 1
+
+print("End of while loop")
+
+# Infinite while Loop
+# while(True):
+#     print("This will run forever!")
